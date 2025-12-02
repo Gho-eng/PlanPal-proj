7f98aff7e5a9f52a5d85fe6774c6e07741ad84da
iniegoleobargas-24-lang <explosionneko71@gmail.com>
2025-12-02 13:01:24 +0800

    feat/ added category table

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,21 +4,16 @@ import cors from "cors";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { db } from "./utils/prisma";
-import { User, UserLogin } from "../types/Users"; 
+import { Expense, User, UserLogin, Category } from "../types/Users"; 
 
 let database: User[] = [];
 
-const app = express();
+const app = express().use(cors({ exposedHeaders: ["Authorization"] })).use(express.json()).use(express.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 3000;
 
-app
-    .use(cors({ exposedHeaders: ["Authorization"] }))
-    .use(express.json())
-    .use(express.urlencoded({ extended: true }))
-    .post("/signup",(req, res) => {
-        const user = req.body as User
-        console.log(user);
-        
+
+const signupInput = app.post("/signup",(req, res) => {
+        const user = req.body as User  
         bcrypt.hash(user.password, 10).then((hashedPwd) => {
             return db.user.create({ data: { 
                 email: user.email, 
@@ -32,14 +27,11 @@ app
             .catch((error) => {
                 console.log(error);
                 res.json({ success: false, error });
-
-
         })});
-    })
-    .post("/login",(req, res) => {
+    });
+const login = app.post("/login",(req, res) => {
         const user = req.body as UserLogin
         const dbUser = database.find((u) => u.email === user.email);
-
         if (dbUser) {
             bcrypt
                 .compare(user.password, dbUser.password)
@@ -55,10 +47,9 @@ app
                     } else res.json({ success: false, error: "user not found" });
                 })
                 .catch((err) => ({ success: false, error: err }));
-
         } else res.json({ success: false, error: "user not found" });
-    })
-    .get("/getUsers", (req, res) => {
+    });
+const getUsers = app.get("/getUsers", (req, res) => {
         db.user.findMany({
         select: {
             id: true,
@@ -69,8 +60,8 @@ app
         .then((result) => {
             res.json({ success: true, data: result });
         });
-    })
-    .get("/getUserId", (req, res) => {
+    });
+const getUserId = app.get("/getUserId", (req, res) => {
         db.user
         .findUnique({where: {id: Number((req.query.id)) } })
         .then((result) => {
@@ -79,22 +70,54 @@ app
                 email: result!.email, 
                 username: result!.username,
                 createdAt: result!.createdAt
-            } });
+            }});
         });
-    })
-    .get("/getExpenses", (req, res) => {
+    });
+const categoryInput = app.post("/category",(req, res) => {
+        const cat = req.body as Category  
+        db.categoryData.create({ data: { 
+            name:   cat.name,
+            user:   { connect: { id: cat.userId } },
+            desc:   cat.desc,
+        }})
+        .then((result) => {
+            res.json({ success: true });
+        })
+        .catch((error) => {
+            console.log(error);
+            res.json({ success: false, error });
+        })}
+    );
+const expenseInput = app.post("/expense",(req, res) => {
+        const exp = req.body as Expense  
+        db.expenseData.create({ data: { 
+            name:   exp.name,
+            amount: exp.amount, 
+            user: { connect: { id: exp.userId } },
+            category: { connect: { id: exp.cat_id } }
+        }})
+        .then((result) => {
+            res.json({ success: true });
+        })
+        .catch((error) => {
+            console.log(error);
+            res.json({ success: false, error });
+        })}
+    );
+const expensesData = app.get("/getExpenses", (req, res) => {
+        db.expenseData.findMany().then((result) => {
+            res.json({ success: true, data: result });
+        });
+    });
+const goalData = app.get("/getGoal", (req, res) => {
         db.goalData.findMany().then((result) => {
             res.json({ success: true, data: result });
         });
     })
-    .get("/getGoal", (req, res) => {
-        db.user.findMany().then((result) => {
-            res.json({ success: true, data: result });
-        });
-    })
-    .listen(PORT, () => {
-        console.log(`Server running on http://localhost:${PORT}`);
-    });
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
 
 
 
--- backend/types/Users.ts
+++ backend/types/Users.ts
@@ -8,3 +8,20 @@ export interface UserLogin {
     email:      string;
     password:   string;
 };
+
+export interface Expense {
+    name:       string;
+    amount:     number;
+    userId:     number;
+    cat_id:     number;
+};
+
+export interface Category {
+    name:       string;
+    desc:       string;
+    userId:     number;
+}
+// export interface UserLogin {
+//     email:      string;
+//     password:   string;
+// };
